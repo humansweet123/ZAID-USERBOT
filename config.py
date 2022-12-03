c56fc5932d2b94cf5f4b164e5c556ae23a6578a9
humansweet123 <119744735+humansweet123@users.noreply.github.com>
2022-12-03 21:59:04 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ from os import getenv
 API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5480218118").split()))
+OWNER_ID = int(getenv("OWNER_ID", "5480218118"))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://humansweet:dewimur123@cluster0.vuehb51.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "5841954743:AAFkYLhHD_5VEmjQpK4ds3w5WTO_rodGAd8")
 ALIVE_PIC = getenv("ALIVE_PIC")
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQBiMZkAjnaUPgEruZjQAmHGDY5Y_S5akIrSXcqn7BG0Al0Ko96cTZn8MwiS_kmJdimjfqh8gOvXZtJIiB7W7N_R-kPaXa0akUS6Vi-CtYOe2FfEyEgzuPbLnrGSVossIy5xzVEW1VKdSu8bhrfKPfKoEwJYqNim74bbIrh4IGPlhRd4nh6VIPjhOJvy1Gnl_ozgGtcxajfl1FT1j_9aerOyq9oLZN-SlJoQ3hfqqlGur10nFqXNhg6HRuEkLsRSpOphn4zAIlM2XrvZm4Wr--bfPupuIqhsClhCBFW-LxxSQaQRblWslVamfW16C65lDqbmM508eoyGwJj1NMw9wfoKvHlvEAAAAAFGpX4GAA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
